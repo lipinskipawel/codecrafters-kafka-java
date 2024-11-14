b8cc5d5b649b588e0039901bd533ef752c0cc971
Pawel Lipinski <lipinskijob@gmail.com>
2024-11-14 16:40:27 +0100

    Serial requests

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -21,38 +21,38 @@ public class Main {
             serverSocket.setReuseAddress(true);
 
             final var clientSocket = serverSocket.accept();
+            while (true) {
 
-            final var requestParser = new RequestParser();
-            final var request = requestParser.parseRequest(clientSocket.getInputStream());
+                final var requestParser = new RequestParser();
+                final var request = requestParser.parseRequest(clientSocket.getInputStream());
 
-            if (request.requestApiVersion() < 0 || request.requestApiVersion() > 4) {
-                final var response = errorResponse(request.correlationId(), new byte[]{0, 35});
+                if (request.requestApiVersion() < 0 || request.requestApiVersion() > 4) {
+                    final var response = errorResponse(request.correlationId(), new byte[]{0, 35});
+
+                    final var outputStream = clientSocket.getOutputStream();
+                    outputStream.write(response);
+                    outputStream.flush();
+                    return;
+                }
+                final var response = response()
+                    .correlationId(request.correlationId())
+                    .errorCode(new byte[]{0, 0})
+                    .apiKeys(new ApiKeys(List.of(
+                        new ApiKey(
+                            new byte[]{0, 18},
+                            new byte[]{0, 3},
+                            new byte[]{0, 4}
+                        )
+                    )))
+                    .throttleTimeMs(new byte[]{0, 0, 0, 0})
+                    .build();
 
                 final var outputStream = clientSocket.getOutputStream();
-                outputStream.write(response);
+                outputStream.write(response.toByteArray());
                 outputStream.flush();
-                Thread.sleep(100);
-                return;
             }
-            final var response = response()
-                .correlationId(request.correlationId())
-                .errorCode(new byte[]{0, 0})
-                .apiKeys(new ApiKeys(List.of(
-                    new ApiKey(
-                        new byte[]{0, 18},
-                        new byte[]{0, 3},
-                        new byte[]{0, 4}
-                    )
-                )))
-                .throttleTimeMs(new byte[]{0, 0, 0, 0})
-                .build();
 
-            final var outputStream = clientSocket.getOutputStream();
-            outputStream.write(response.toByteArray());
-            outputStream.flush();
-            Thread.sleep(100);
-
-        } catch (IOException | InterruptedException e) {
+        } catch (IOException e) {
             System.err.println("IOException: " + e.getMessage());
         }
     }
